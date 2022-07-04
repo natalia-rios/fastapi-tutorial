bd05c3b1c5e17b5a89992b68d1c6a41003c9868c
riosnatalia <natalia.rios@wexinc.com>
2022-07-04 11:56:23 -0300

    request body

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from enum import Enum
+from typing import Union
 
+from pydantic import BaseModel
 from fastapi import FastAPI
 
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+
 
 app = FastAPI()
-
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
 @app.get("/")
@@ -18,7 +19,7 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: str, needy: str, skip: int = 0, limit: int  | None = None):
+async def read_item(item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None):
     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
 
@@ -30,27 +31,13 @@ async def read_user_me():
 async def read_user(user_id: str):
     return {"user_id": user_id}
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
 
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
-
 @app.get("/users/{user_id}/items/{item_id}")
 async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
 ):
     item = {"item_id": item_id, "owner_id": user_id}
     if q:
@@ -60,3 +47,18 @@ async def read_user_item(
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
